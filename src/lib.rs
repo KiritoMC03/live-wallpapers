46b6fbc05c0b09be853599e699b70b0aac2fbf9c
Dudnik Kirill <prog.me.2017@gmail.com>
2023-06-05 18:57:32 +0200

    - refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
-use std::default::Default;
-use core::ptr::null_mut;
+pub use std::default::Default;
+pub use core::ptr::null_mut;
 
-use winapi::ctypes::c_int;
-use winapi::shared::basetsd::LONG_PTR;
-use winapi::shared::minwindef::BOOL;
+pub use winapi::ctypes::c_int;
+pub use winapi::shared::basetsd::LONG_PTR;
+pub use winapi::shared::minwindef::BOOL;
 
-use winapi::um::errhandlingapi::GetLastError;
-use winapi::um::libloaderapi::GetModuleHandleW;
+pub use winapi::um::errhandlingapi::GetLastError;
+pub use winapi::um::libloaderapi::GetModuleHandleW;
 
-use winapi::um::wingdi::{RGB, CreateSolidBrush, GetRValue, GetBValue, GetGValue};
-use winapi::um::winuser::{
+pub use winapi::um::wingdi::{RGB, CreateSolidBrush, GetRValue, GetBValue, GetGValue};
+pub use winapi::um::winuser::{
     WNDCLASSW,
+    WNDPROC,
     PAINTSTRUCT,
     CREATESTRUCTW,
     MSG,
@@ -31,7 +32,7 @@ use winapi::um::winuser::{
     COLOR_HIGHLIGHT, GCLP_HBRBACKGROUND, SetClassLongPtrA,
 };
 
-use winapi::um::winuser::{
+pub use winapi::um::winuser::{
     DefWindowProcW,
     RegisterClassW,
     CreateWindowExW,
@@ -54,25 +55,25 @@ use winapi::um::winuser::{
     LoadCursorW,
 };
 
-use winapi::um::winuser::{
+pub use winapi::um::winuser::{
     GetMessageW,
     TranslateMessage,
     DispatchMessageW,
     SystemParametersInfoW,
 };
 
-use winapi::um::winuser::{
+pub use winapi::um::winuser::{
     BeginPaint,
     FillRect,
     EndPaint,
 };
 
-use winapi::shared::windef::{
+pub use winapi::shared::windef::{
     HWND,
     HBRUSH,
 };
 
-use winapi::shared::minwindef::{
+pub use winapi::shared::minwindef::{
     UINT,
     HINSTANCE,
     WPARAM,
@@ -80,7 +81,7 @@ use winapi::shared::minwindef::{
     LRESULT,
 };
 
-use winapi::um::winuser::{
+pub use winapi::um::winuser::{
     SW_SHOW,
 
     WM_CLOSE,
@@ -92,11 +93,11 @@ pub const WORKER_W_STR : &str = "WorkerW";
 
 static mut WORKER_W : HWND = null_mut();
 
-pub fn create_window_class(name: &Vec<u16>) -> (WNDCLASSW, HINSTANCE) {
+pub fn create_window_class(name: &Vec<u16>, window_procedure: WNDPROC) -> (WNDCLASSW, HINSTANCE) {
     let h_instance = unsafe { GetModuleHandleW(core::ptr::null()) };
 
     let mut wc = WNDCLASSW::default();
-    wc.lpfnWndProc = Some(window_procedure);
+    wc.lpfnWndProc = window_procedure;
     wc.hInstance = h_instance;
     wc.lpszClassName = name.as_ptr();
     wc.hCursor = unsafe { LoadCursorW(null_mut(), IDC_ARROW) };
@@ -225,97 +226,6 @@ pub fn handle_window_messages(mut msg: MSG) {
     }
 }
 
-static mut FRAME : u128 = 0;
-
-pub unsafe extern "system" fn window_procedure(hwnd: HWND, msg: UINT, w_param: WPARAM, l_param: LPARAM,) -> LRESULT {
-    match msg {
-        WM_NCCREATE => {
-            println!("NC Create");
-            let createstruct: *mut CREATESTRUCTW = l_param as *mut _;
-            if createstruct.is_null() {
-                return 0;
-            }
-            let boxed_i32_ptr = (*createstruct).lpCreateParams;
-            SetWindowLongPtrW(hwnd, GWLP_USERDATA, boxed_i32_ptr as LONG_PTR);
-            return 1;
-        }
-        WM_CREATE => println!("WM Create"),
-        WM_CLOSE => drop(DestroyWindow(hwnd)),
-        WM_DESTROY => {
-            let ptr = GetWindowLongPtrW(hwnd, GWLP_USERDATA) as *mut i32;
-            drop(Box::from_raw(ptr));
-            println!("Cleaned up the box.");
-            PostQuitMessage(0);
-        }
-        WM_PAINT => {
-//            println!("Paint !!!");
-
-
-            let colors = [
-                CUSTOM_RGB::new(255, 0, 0),   // Red
-                CUSTOM_RGB::new(0, 255, 0),   // Green
-                CUSTOM_RGB::new(0, 0, 255),   // Blue
-                CUSTOM_RGB::new(255, 255, 0), // Yellow
-            ];
-
-
-            let mut ps: PAINTSTRUCT = std::mem::zeroed();
-            let hdc = BeginPaint(hwnd, &mut ps);
-            let rect = &ps.rcPaint;
-
-            // Fill the background with a specific color (e.g., blue)
-            let color = interpolate_colors(&colors, (FRAME % 600 as u128) as f32 / 600_f32);
-            println!("unpacked: {}-{}-{}", GetRValue(color), GetGValue(color) ,GetBValue(color));
-            //            println!("col: {}, {}", color, FRAME);
-            let hbrush = CreateSolidBrush(color);
-            FillRect(hdc, rect, hbrush);
-
-            EndPaint(hwnd, &ps);
-            FRAME += 1;
-
-
-
-//            let ptr = GetWindowLongPtrW(hwnd, GWLP_USERDATA) as *mut i32;
-//            println!("Current ptr: {}", *ptr);
-//            *ptr += 1;
-//            let mut ps = PAINTSTRUCT::default();
-//            let hdc = BeginPaint(hwnd, &mut ps);
-//
-//            let brush_color = RGB(255, 0, 0); // Color is set to red (change as needed)
-//            let _success = FillRect(hdc, &ps.rcPaint, (brush_color) as HBRUSH);
-//            EndPaint(hwnd, &ps);
-        }
-        _ => return DefWindowProcW(hwnd, msg, w_param, l_param),
-    }
-    
-    0
-}
-
 pub fn wide_null(s: &str) -> Vec<u16> {
     s.encode_utf16().chain(Some(0)).collect()
-}
-
-type CUSTOM_RGB = rgb::RGB<u8>;
-
-fn interpolate_colors(colors: &[CUSTOM_RGB], weight: f32) -> u32 {
-    let num_colors = colors.len();
-    let segment = 1.0 / (num_colors - 1) as f32;
-
-    // Find the two adjacent colors for the given weight
-    let index1 = (weight / segment).floor() as usize;
-    let index2 = index1 + 1;
-
-    let color1 = colors[index1];
-    let color2 = colors[index2];
-
-    // Calculate the weight within the segment
-    let segment_weight = (weight - index1 as f32 * segment) / segment;
-
-    // Interpolate between the two colors
-    let r = ((1.0 - segment_weight) * color1.r as f32 + segment_weight * color2.r as f32) as u8;
-    let g = ((1.0 - segment_weight) * color1.g as f32 + segment_weight * color2.g as f32) as u8;
-    let b = ((1.0 - segment_weight) * color1.b as f32 + segment_weight * color2.b as f32) as u8;
-
-    println!("{}-{}-{}", r, g ,b);
-    RGB(r, g, b)
 }
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,14 @@ use winapi::um::winuser::{MSG, InvalidateRect, RedrawWindow};
 use live_wallpapers::*;
 
 
+type CUSTOM_RGB = rgb::RGB<u8>;
+
+static mut FRAME : u128 = 0;
+
 fn main() {
     let class_name = wide_null("Window Class Name");
     let window_name = wide_null("Window Name");
-    let (window_class, h_instance) = create_window_class(&class_name/*, Some(window_procedure)*/);
+    let (window_class, h_instance) = create_window_class(&class_name, Some(window_procedure));
     let window_handle = create_window_handle(&window_class, &class_name, &window_name, h_instance);
     let _window = create_window(window_handle);
 
@@ -30,4 +34,77 @@ fn main() {
         unsafe { InvalidateRect(window_handle, null_mut(), 0) };
         unsafe { RedrawWindow(window_handle, null_mut(), null_mut(), 0) };
     }
+}
+
+pub unsafe extern "system" fn window_procedure(hwnd: HWND, msg: UINT, w_param: WPARAM, l_param: LPARAM,) -> LRESULT {
+    match msg {
+        WM_NCCREATE => {
+            println!("NC Create");
+            let createstruct: *mut CREATESTRUCTW = l_param as *mut _;
+            if createstruct.is_null() {
+                return 0;
+            }
+            let boxed_i32_ptr = (*createstruct).lpCreateParams;
+            SetWindowLongPtrW(hwnd, GWLP_USERDATA, boxed_i32_ptr as LONG_PTR);
+            return 1;
+        }
+        WM_CREATE => println!("WM Create"),
+        WM_CLOSE => drop(DestroyWindow(hwnd)),
+        WM_DESTROY => {
+            let ptr = GetWindowLongPtrW(hwnd, GWLP_USERDATA) as *mut i32;
+            drop(Box::from_raw(ptr));
+            println!("Cleaned up the box.");
+            PostQuitMessage(0);
+        }
+        WM_PAINT => {
+            simulate_frame(hwnd);
+        }
+        _ => return DefWindowProcW(hwnd, msg, w_param, l_param),
+    }
+
+    0
+}
+
+fn simulate_frame(hwnd: HWND) {
+    let colors = [
+        CUSTOM_RGB::new(226, 239, 84),
+        CUSTOM_RGB::new(255, 92, 102),
+        CUSTOM_RGB::new(98, 72, 213),
+        CUSTOM_RGB::new(226, 239, 84),
+    ];
+
+
+    let mut ps: PAINTSTRUCT = PAINTSTRUCT::default();
+    let hdc = unsafe { BeginPaint(hwnd, &mut ps) };
+    let rect = &ps.rcPaint;
+
+    // Fill the background with a specific color (e.g., blue)
+    let color = interpolate_colors(&colors, (unsafe { FRAME } % 200 as u128) as f32 / 200_f32);
+    let hbrush = unsafe { CreateSolidBrush(color) };
+    unsafe { FillRect(hdc, rect, hbrush) };
+    unsafe { EndPaint(hwnd, &ps) };
+    unsafe { FRAME += 1 };
+
+}
+
+pub fn interpolate_colors(colors: &[CUSTOM_RGB], weight: f32) -> u32 {
+    let num_colors = colors.len();
+    let segment = 1.0 / (num_colors - 1) as f32;
+
+    // Find the two adjacent colors for the given weight
+    let index1 = (weight / segment).floor() as usize;
+    let index2 = index1 + 1;
+
+    let color1 = colors[index1];
+    let color2 = colors[index2];
+
+    // Calculate the weight within the segment
+    let segment_weight = (weight - index1 as f32 * segment) / segment;
+
+    // Interpolate between the two colors
+    let r = ((1.0 - segment_weight) * color1.r as f32 + segment_weight * color2.r as f32) as u8;
+    let g = ((1.0 - segment_weight) * color1.g as f32 + segment_weight * color2.g as f32) as u8;
+    let b = ((1.0 - segment_weight) * color1.b as f32 + segment_weight * color2.b as f32) as u8;
+
+    RGB(r, g, b)
 }
