ca1dd47134290cfdae90413937da6440342cfefb
Dudnik Kirill <prog.me.2017@gmail.com>
2023-06-11 22:34:58 +0200

    - refactoring

--- src/main.rs
+++ src/main.rs
@@ -18,27 +18,55 @@ use live_wallpapers::drawing::{
     colors::*,
 };
 
-static mut FRAME : u128 = 0;
-static mut BG_STEPS : u128 = 0;
-static mut FRAME_PROCESSED : bool = false;
-static mut CURRENT_GALAXY : Galaxy = Galaxy::empty();
-const WIDTH: usize = 1920;
-const HEIGHT: usize = 1080;
+static mut APP_DATA : AppData = AppData{
+    width: 0,
+    height: 0,
+    frame_num: 0,
+    frame_processed: false,
+    current_galaxy: Galaxy::empty(),
+    bg_progress: 0,
+};
+
+struct AppData {
+    width: usize,
+    height: usize,
+    frame_num: u128,
+    frame_processed: bool,
+    current_galaxy: Galaxy,
+    bg_progress: u128,
+}
 
 fn main() {
+    unsafe {
+        APP_DATA = AppData {
+            width: GetSystemMetrics(SM_CXSCREEN) as usize,
+            height: GetSystemMetrics(SM_CXSCREEN) as usize,
+            frame_num: 0,
+            frame_processed: false,
+            current_galaxy: Galaxy::empty(),
+            bg_progress: 0,
+        }
+    }
     let window_handle = create_desktop_window_fast("Live", Some(window_procedure));
 
     let msg = MSG::default();
+    let app_data = ref_app_data();
     loop { // ToDo: stop on app close
-        if handle_window_messages(msg){ }
-        else {
-            unsafe {
-                if !FRAME_PROCESSED {
-                    RedrawWindow(window_handle, null_mut(), null_mut(), RDW_INVALIDATE);
-                }
-            }
+        if handle_window_messages(msg) { }
+        else if !app_data.frame_processed {
+            unsafe { RedrawWindow(window_handle, null_mut(), null_mut(), RDW_INVALIDATE); }
         }
+
+        std::thread::sleep(std::time::Duration::from_micros(16666));
     }
+}
+
+fn ref_app_data() -> &'static AppData {
+    unsafe { &APP_DATA }
+}
+
+fn mut_app_data() -> &'static mut AppData {
+    unsafe { &mut APP_DATA }
 }
 
 pub unsafe extern "system" fn window_procedure(hwnd: HWND, msg: UINT, w_param: WPARAM, l_param: LPARAM,) -> LRESULT {
@@ -61,20 +89,19 @@ pub unsafe extern "system" fn window_procedure(hwnd: HWND, msg: UINT, w_param: W
             println!("Cleaned up the box.");
             PostQuitMessage(0);
         }
-        WM_PAINT => simulate_frame(hwnd),
+        WM_PAINT => simulate_frame(hwnd, mut_app_data()),
         _ => return DefWindowProcW(hwnd, msg, w_param, l_param),
     }
 
     0
 }
 
-fn simulate_frame(hwnd: HWND) {
-    unsafe {
-        if FRAME_PROCESSED {
-            return;
-        }
-        FRAME_PROCESSED = true;
+fn simulate_frame(hwnd: HWND, app: &mut AppData) {
+    if app.frame_processed {
+        return;
     }
+    app.frame_processed = true;
+
     let colors = [
         RGB::new(226, 239, 84),
         RGB::new(255, 92, 102),
@@ -91,24 +118,21 @@ fn simulate_frame(hwnd: HWND) {
 
     let mouse_x = p.x as f64;
     let mouse_y = p.y as f64;
-    unsafe {
-        if CURRENT_GALAXY.x != mouse_x || CURRENT_GALAXY.y != mouse_y {
-            let color = interpolate_colors(&colors, (BG_STEPS % 200 as u128) as f32 / 200_f32);
-            draw_fullscreen_rect(hdc, &ps, color);
-            BG_STEPS += 1;
-            let c = random_color();
-            CURRENT_GALAXY = Galaxy::new(mouse_x, mouse_y, WIDTH, HEIGHT, c);
-        }
+        if app.current_galaxy.x != mouse_x || app.current_galaxy.y != mouse_y {
+        let color = interpolate_colors(&colors, (app.bg_progress % 200 as u128) as f32 / 200_f32);
+        draw_fullscreen_rect(hdc, &ps, color);
+        app.bg_progress += 1;
+        let c = random_color();
+        app.current_galaxy = Galaxy::new(mouse_x, mouse_y, app.width, app.height, c);
     }
 
     let hello = wide_null("Hello, Windows!");
     unsafe { TextOutW(hdc, 0, 0, hello.as_ptr(), 15) };
-    draw_galaxy_step_inc(hdc, unsafe { &mut CURRENT_GALAXY });
+    draw_galaxy_step_inc(hdc, &mut app.current_galaxy);
 
     unsafe {
         EndPaint(hwnd, &ps);
-        FRAME += 1;
-        FRAME_PROCESSED = false;
+        app.frame_num += 1;
+        app.frame_processed = false;
     };
 }
-
